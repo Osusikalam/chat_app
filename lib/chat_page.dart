2f3170e94d0700aa6734adc426428986e8129fdd
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-30 00:50:43 +0800

    Exploring provider way

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'models/chat_message_entity.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
@@ -83,15 +84,12 @@ class _ChatPageState extends State<ChatPage> {
                   itemCount: _messages.length,
                   itemBuilder: (context, index) {
                     return ChatBubble(
-                        alignment:
-                        _messages[index].author.userName == 'poojab26'
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
                         alignment: _messages[index].author.userName ==
                             AuthService().getUserName()
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        entity: _messages[index]);
+                        context.read<AuthService>().getUserName()
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    entity: _messages[index]);
                   })),
           ChatInput(
             onSubmit: onMessageSent,
--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,119 +1,107 @@
 import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:chat_app/widgets/picker_body.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ChatInput extends StatelessWidget {
-  class ChatInput extends StatefulWidget {
+class ChatInput extends StatefulWidget {
   final Function(ChatMessageEntity) onSubmit;
   ChatInput({Key? key, required this.onSubmit}) : super(key: key);
 
   @override
   State<ChatInput> createState() => _ChatInputState();
-  }
+}
 
-  class _ChatInputState extends State<ChatInput> {
+class _ChatInputState extends State<ChatInput> {
   String _selectedImageUrl = '';
 
   final chatMessageController = TextEditingController();
 
   void onSendButtonPressed() {
-  print('Chat message: ${chatMessageController.text}');
-  //TODO: Add this new message to the default list
-  final newChatMessage = ChatMessageEntity(
-  text: chatMessageController.text,
-  id: "244",
-  createdAt: DateTime.now().millisecondsSinceEpoch,
-  author: Author(userName: 'poojab26'));
+    print('Chat message: ${chatMessageController.text}');
+    //TODO: Add this new message to the default list
+    final newChatMessage = ChatMessageEntity(
+        text: chatMessageController.text,
+        id: "244",
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        author: Author(userName: 'poojab26'));
+    author: Author(userName: context.read<AuthService>().getUserName()));
 
-  onSubmit(newChatMessage);
-  //TODO: Check for image and append it to the chat object
-  if (_selectedImageUrl.isNotEmpty) {
-  newChatMessage.imageUrl = _selectedImageUrl;
-  }
-  widget.onSubmit(newChatMessage);
-  chatMessageController.clear();
-  _selectedImageUrl = '';
-  setState(() {});
+    //TODO: Check for image and append it to the chat object
+    if (_selectedImageUrl.isNotEmpty) {
+    newChatMessage.imageUrl = _selectedImageUrl;
+    }
+    widget.onSubmit(newChatMessage);
+    chatMessageController.clear();
+    _selectedImageUrl = '';
+    setState(() {});
   }
 
   void onImagePicked(String newImageUrl) {
-  setState(() {
-  _selectedImageUrl = newImageUrl;
-  });
-  Navigator.of(context).pop();
+    setState(() {
+      _selectedImageUrl = newImageUrl;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-  return Container(
-  height: 80,
-  child: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: [
-  IconButton(
-  onPressed: () {
-  //TODO: Open a bottom sheet that shows a grid of images
-  showModalBottomSheet(
-  context: context,
-  builder: (BuildContext context) {
-  return NetworkImagePickerBody();
-  return NetworkImagePickerBody(
-  onImageSelected: onImagePicked,
-  );
-  });
-  },
-  icon: Icon(
-  Icons.add,
-  color: Colors.white,
-  ),
-  ),
-  //TODO: Add multi-line Textfield here
-  Expanded(
-  child: TextField(
-  keyboardType: TextInputType.multiline,
-  maxLines: 5,
-  minLines: 1,
-  controller: chatMessageController,
-  textCapitalization: TextCapitalization.sentences,
-  style: TextStyle(color: Colors.white),
-  decoration: InputDecoration(
-  hintText: "Type your message",
-  hintStyle: TextStyle(color: Colors.blueGrey),
-  border: InputBorder.none),
-  child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-  TextField(
-  keyboardType: TextInputType.multiline,
-  maxLines: 5,
-  minLines: 1,
-  controller: chatMessageController,
-  textCapitalization: TextCapitalization.sentences,
-  style: TextStyle(color: Colors.white),
-  decoration: InputDecoration(
-  hintText: "Type your message",
-  hintStyle: TextStyle(color: Colors.blueGrey),
-  border: InputBorder.none),
-  ),
-  if (_selectedImageUrl.isNotEmpty)
-  Image.network(
-  _selectedImageUrl,
-  height: 50,
-  ),
-  ],
-  )),
-  IconButton(
-  onPressed: onSendButtonPressed,
-  icon: Icon(
-  Icons.send,
-  color: Colors.white,
-  ),
-  ),
-  ],
-  ),
-  decoration: BoxDecoration(
-  color: Colors.black,
-  borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-  );
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              //TODO: Open a bottom sheet that shows a grid of images
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return NetworkImagePickerBody(
+                      onImageSelected: onImagePicked,
+                    );
+                  });
+            },
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          //TODO: Add multi-line Textfield here
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 5,
+                    minLines: 1,
+                    controller: chatMessageController,
+                    textCapitalization: TextCapitalization.sentences,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        hintText: "Type your message",
+                        hintStyle: TextStyle(color: Colors.blueGrey),
+                        border: InputBorder.none),
+                  ),
+                  if (_selectedImageUrl.isNotEmpty)
+                    Image.network(
+                      _selectedImageUrl,
+                      height: 50,
+                    ),
+                ],
+              )),
+          IconButton(
+            onPressed: onSendButtonPressed,
+            icon: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+    );
   }
-  }
+}
