218294b98794f94c8eb1b239ac8d55ad3401b400
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-29 23:40:43 +0800

    Passing data with navigation

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -3,42 +3,45 @@ import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  final String username;
+  const ChatPage({Key? key, required this.username}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Center(
-            child: Text('Hi Jonjie!'),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  print('Icon pressed!');
-                },
-                icon: Icon(Icons.logout))
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+
+        title: Center(
+          child: Text('Hi $username!'),
         ),
-        body: Column(
-            children: [
-        Expanded(
-        child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return ChatBubble(
-              alignment: index % 2 == 0
-                  ? Alignment.centerLeft
-                  : Alignment.centerRight,
-              message: "Hello, this is Ivan!");
-        }),
-    ),
-    ChatInput(),
-    ],
-    ),
+
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+                print('Icon pressed!');
+              },
+              icon: Icon(Icons.logout))
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ChatBubble(
+                      alignment: index % 2 == 0
+                          ? Alignment.centerLeft
+                          : Alignment.centerRight,
+                      message: "Hello, this is Ivan!");
+                }),
+          ),
+          ChatInput(),
+        ],
+      ),
     );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,120 +8,121 @@ class LoginPage extends StatelessWidget {
 
   final _formkey = GlobalKey<FormState>();
 
-    void loginUser(context) {
-      if (_formkey.currentState != null && _formkey.currentState!.validate()) {
-        print(userNameController.text);
-        print(passwordController.text);
+  void loginUser(context) {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
 
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ChatPage()));
-        print('login! successful!');
-      } else {
-        print('not successful');
-      }
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+      builder: (context) => ChatPage(
+        username: userNameController.text,)));
+    print('login! successful!');
+    } else {
+    print('not successful');
     }
+  }
 
-    final userNameController = TextEditingController();
-    final passwordController = TextEditingController();
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-          body: Center(
-          child: Padding(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
           padding: const EdgeInsets.all(24.0),
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-      Text(
-      'Let\'s sign you in!',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-      fontSize: 30,
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-      letterSpacing: 0.5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 200,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value != null &&
+                            value.isNotEmpty &&
+                            value.length < 5) {
+                          return "Your username should be more than 5 characters";
+                        } else if (value != null && value.isEmpty) {
+                          return "Please type your username";
+                        }
+                        return null;
+                      },
+                      controller: userNameController,
+                      decoration: InputDecoration(
+                          hintText: 'Add your username',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: 'Type your password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder()),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  loginUser(context);
+                },
+                child: Text('Login',
+                    style:
+                    TextStyle(fontSize: 24, fontWeight: FontWeight.w300)),
+              ),
+              InkWell(
+                splashColor: Colors.red,
+                onDoubleTap: () {
+                  print('double tapped!');
+                },
+                onLongPress: () {
+                  print('onLongpress!');
+                },
+                onTap: () {
+                  print('Link clicked!');
+                },
+                child: Column(
+                  children: [
+                    Text('Find us on'),
+                    Text('https://google.com'),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      Text(
-      'Welcome back! \n You\'ve been missed!',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-      fontWeight: FontWeight.w500,
-      fontSize: 20,
-      color: Colors.blueGrey),
-      ),
-      Image.network(
-      'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-      height: 200,
-      ),
-      Form(
-      key: _formkey,
-      child: Column(
-      children: [
-      TextFormField(
-      validator: (value) {
-      if (value != null &&
-      value.isNotEmpty &&
-      value.length < 5) {
-      return "Your username should be more than 5 characters";
-      } else if (value != null && value.isEmpty) {
-      return "Please type your username";
-      }
-      return null;
-      },
-      controller: userNameController,
-      decoration: InputDecoration(
-      hintText: 'Add your username',
-      hintStyle: TextStyle(color: Colors.blueGrey),
-      border: OutlineInputBorder()),
-      ),
-      SizedBox(
-      height: 24,
-      ),
-      TextFormField(
-      controller: passwordController,
-      obscureText: true,
-      decoration: InputDecoration(
-      hintText: 'Type your password',
-      hintStyle: TextStyle(color: Colors.blueGrey),
-      border: OutlineInputBorder()),
-      ),
-      ],
-      ),
-      ),
-      ElevatedButton(
-      onPressed: () {
-      loginUser(context);
-      },
-      child: Text('Login',
-      style:
-      TextStyle(fontSize: 24, fontWeight: FontWeight.w300)),
-      ),
-      InkWell(
-      splashColor: Colors.red,
-      onDoubleTap: () {
-      print('double tapped!');
-      },
-      onLongPress: () {
-      print('onLongpress!');
-      },
-      onTap: () {
-      print('Link clicked!');
-      },
-      child: Column(
-      children: [
-      Text('Find us on'),
-      Text('https://google.com'),
-      ],
-      ),
-      )
-      ],
-      ),
-      ),
-      ),
-      );
-      }
-    }
+    );
+  }
+}
