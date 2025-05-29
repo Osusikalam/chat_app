a094c0d184431e387bf825b45605abe47fc88b4e
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-30 00:18:01 +0800

    Modeling entities in Dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,8 @@
-//import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/utils/spaces.dart';
 //import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_buttons/social_media_button.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
@@ -33,97 +33,110 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+      body: Center(
         child: Padding(
-        padding: const EdgeInsets.all(24.0),
-    child: SingleChildScrollView(
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [
-    Text(
-    'Let\'s sign you in!',
-    textAlign: TextAlign.center,
-    style: TextStyle(
-    fontSize: 30,
-    color: Colors.black,
-    fontWeight: FontWeight.bold,
-    letterSpacing: 0.5),
-    ),
-    Text(
-    'Welcome back! \n You\'ve been missed!',
-    textAlign: TextAlign.center,
-    style: TextStyle(
-    fontWeight: FontWeight.w500,
-    fontSize: 20,
-    color: Colors.blueGrey),
-    ),
-    Image.asset(
-    'assets/illustration.png',
-    height: 200,
-    ),
+          padding: const EdgeInsets.all(24.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Let\'s sign you in!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.5),
+                ),
+                Text(
+                  'Welcome back! \n You\'ve been missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.blueGrey),
+                ),
+                Image.asset(
+                  'assets/illustration.png',
+                  height: 200,
+                ),
 
-    //TODO: Add Username & Password text fields
+                //TODO: Add Username & Password text fields
 
-    Form(
-    key: _formkey,
-    child: Column(
-    children: [
-    LoginTextField(
-    hintText: "Enter your username",
-    validator: (value) {
-    if (value != null &&
-    value.isNotEmpty &&
-    value.length < 5) {
-    return "Your username should be more than 5 characters";
-    } else if (value != null && value.isEmpty) {
-    return "Please type your username";
-    }
-    return null;
-    },
-    controller: userNameController,
-    ),
-    verticalSpacing(24),
-    LoginTextField(
-    hasAsterisks: true,
-    controller: passwordController,
-    hintText: 'Enter your password',
-    ),
-    ],
-    ),
-    ),
-    verticalSpacing(24),
-    ElevatedButton(
-    onPressed: () {
-    loginUser(context);
-    },
-    child: Text(
-    'Login',
-    style:
-    TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
-    )),
-    GestureDetector(
-    onTap: () {
-    onTap: () async {
-    //todo: Navigate to browser
-    print('Link clicked!');
-    if (!await launch(_mainUrl)) {
-    throw 'Could not launch this';
-    }
-    },
-    child: Column(
-    children: [
-    Text('Find us on'),
-    Text('https://poojabhaumik.com'),
-    Text(_mainUrl),
-    ],
-    ),
-    )
-    ],
-    ),
-    ),
-    ),
-    ),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      LoginTextField(
+                        hintText: "Enter your username",
+                        validator: (value) {
+                          if (value != null &&
+                              value.isNotEmpty &&
+                              value.length < 5) {
+                            return "Your username should be more than 5 characters";
+                          } else if (value != null && value.isEmpty) {
+                            return "Please type your username";
+                          }
+                          return null;
+                        },
+                        controller: userNameController,
+                      ),
+                      verticalSpacing(24),
+                      LoginTextField(
+                        hasAsterisks: true,
+                        controller: passwordController,
+                        hintText: 'Enter your password',
+                      ),
+                    ],
+                  ),
+                ),
+                verticalSpacing(24),
+                ElevatedButton(
+                    onPressed: () {
+                      loginUser(context);
+                    },
+                    child: Text(
+                      'Login',
+                      style:
+                      TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                    )),
+                GestureDetector(
+                  onTap: () async {
+                    //todo: Navigate to browser
+                    print('Link clicked!');
+                    if (!await launch(_mainUrl)) {
+                      throw 'Could not launch this';
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      Text('Find us on'),
+                      Text(_mainUrl),
+                    ],
+                  ),
+                ),
+                //TODO: Add Social media buttons here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialMediaButton.facebook(
+                      size: 20,
+                      color: Colors.blue,
+                      url: "https://www.facebook.com/share/1BPmT72qMA/",
+                    ),
+                    SocialMediaButton.linkedin(
+                      url:
+                      "https://www.linkedin.com/in/alvic-caranzo-b724a4170/",
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
-    }
   }
+}
