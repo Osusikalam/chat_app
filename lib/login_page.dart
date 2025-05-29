4fff5c2b91fad735b8ac9044b962abf06fb64b6c
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-30 00:15:48 +0800

    Adding plugins to your app from pub dev

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
+//import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/utils/spaces.dart';
 //import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -26,98 +28,102 @@ class LoginPage extends StatelessWidget {
 
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
+  final _mainUrl = 'https://poojabhaumik.com';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+        body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-              Text(
-              'Let\'s sign you in!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-              Image.asset(
-                'assets/illustration.png',
-                height: 200,
-              ),
+        padding: const EdgeInsets.all(24.0),
+    child: SingleChildScrollView(
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+    Text(
+    'Let\'s sign you in!',
+    textAlign: TextAlign.center,
+    style: TextStyle(
+    fontSize: 30,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.5),
+    ),
+    Text(
+    'Welcome back! \n You\'ve been missed!',
+    textAlign: TextAlign.center,
+    style: TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+    color: Colors.blueGrey),
+    ),
+    Image.asset(
+    'assets/illustration.png',
+    height: 200,
+    ),
 
-              //TODO: Add Username & Password text fields
+    //TODO: Add Username & Password text fields
 
-              Form(
-                key: _formkey,
-                child: Column(
-                  children: [
-                    LoginTextField(
-                      hintText: "Enter your username",
-                      validator: (value) {
-                        if (value != null &&
-                            value.isNotEmpty &&
-                            value.length < 5) {
-                          return "Your username should be more than 5 characters";
-                        } else if (value != null && value.isEmpty) {
-                          return "Please type your username";
-                        }
-                        return null;
-                      },
-                      controller: userNameController,
-                    ),
-                    verticalSpacing(24),
-                    LoginTextField(
-                      hasAsterisks: true,
-                      controller: passwordController,
-                      hintText: 'Enter your password',
-                    ),
-                  ],
-                ),
-              ),
-              verticalSpacing(24),
-              ElevatedButton(
-                  onPressed: () {
-                    loginUser(context);
-                  },
-                  child: Text(
-                    'Login',
-                    style:
-                    TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
-                  )),
-              GestureDetector(
-                onTap: () {
-                  //todo: Navigate to browser
-                  print('Link clicked!');
-                },
-                child: Column(
-                  children: [
-                    Text('Find us on'),
-                    Text('https://poojabhaumik.com'),
-                  ],
-                ),
-              )
-              ],
-            ),
-          ),
-        ),
-      ),
+    Form(
+    key: _formkey,
+    child: Column(
+    children: [
+    LoginTextField(
+    hintText: "Enter your username",
+    validator: (value) {
+    if (value != null &&
+    value.isNotEmpty &&
+    value.length < 5) {
+    return "Your username should be more than 5 characters";
+    } else if (value != null && value.isEmpty) {
+    return "Please type your username";
+    }
+    return null;
+    },
+    controller: userNameController,
+    ),
+    verticalSpacing(24),
+    LoginTextField(
+    hasAsterisks: true,
+    controller: passwordController,
+    hintText: 'Enter your password',
+    ),
+    ],
+    ),
+    ),
+    verticalSpacing(24),
+    ElevatedButton(
+    onPressed: () {
+    loginUser(context);
+    },
+    child: Text(
+    'Login',
+    style:
+    TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+    )),
+    GestureDetector(
+    onTap: () {
+    onTap: () async {
+    //todo: Navigate to browser
+    print('Link clicked!');
+    if (!await launch(_mainUrl)) {
+    throw 'Could not launch this';
+    }
+    },
+    child: Column(
+    children: [
+    Text('Find us on'),
+    Text('https://poojabhaumik.com'),
+    Text(_mainUrl),
+    ],
+    ),
+    )
+    ],
+    ),
+    ),
+    ),
+    ),
     );
+    }
   }
-}
