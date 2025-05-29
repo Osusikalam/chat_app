f15f9f5df347048f3732dde5251afec31661387b
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-30 00:27:06 +0800

    Reading data from a JSON file

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,75 +1,100 @@
+import 'dart:convert';
+
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'models/chat_message_entity.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  class ChatPage extends StatefulWidget {
   ChatPage({Key? key}) : super(key: key);
 
   List<ChatMessageEntity> _messages = [
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231242,
-        id: '1',
-        text: 'First text'),
-    ChatMessageEntity(
-        author: Author(userName: 'pooja'),
-        createdAt: 2131231442,
-        id: '1',
-        text: 'Second text',
-        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
-    ChatMessageEntity(
-      author: Author(userName: 'jane'),
-      createdAt: 2131234242,
-      id: '1',
-      text: 'Third text',
-    )
+  ChatMessageEntity(
+  author: Author(userName: 'pooja'),
+  createdAt: 2131231242,
+  id: '1',
+  text: 'First text'),
+  ChatMessageEntity(
+  author: Author(userName: 'pooja'),
+  createdAt: 2131231442,
+  id: '1',
+  text: 'Second text',
+  imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
+  ChatMessageEntity(
+  author: Author(userName: 'jane'),
+  createdAt: 2131234242,
+  id: '1',
+  text: 'Third text',
+  )
   ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+  }
+
+  class _ChatPageState extends State<ChatPage> {
+  //initiate state of messages
+  List<ChatMessageEntity> _messages = [];
+
+  _loadInitialMessages() async {
+  final response = await rootBundle.loadString('assets/mock_messages.json');
+
+  final List<dynamic> decodeList = jsonDecode(response) as List;
+
+  final List<ChatMessageEntity> _chatMessages = decodeList.map((listItem) {
+  return ChatMessageEntity.fromJson(listItem);
+  }).toList();
+
+  print(_chatMessages.length);
+
+  //final state of messages
+  setState(() {
+  _messages = _chatMessages;
+  });
+  }
+
+  @override
+  void initState() {
+  _loadInitialMessages();
+  // TODO: implement initState
+  super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final username = ModalRoute.of(context)!.settings.arguments as String;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text('Hi $username!'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                //TODO: Navigate back to LoginPage on logout
-                Navigator.pushReplacementNamed(context, '/');
-                print('Icon press');
-              },
-              icon: Icon(Icons.logout))
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemCount: _messages.length,
-                  itemBuilder: (context, index) {
-                    return ChatBubble(
-                        alignment: index % 2 == 0
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        entity: ChatMessageEntity(
-                            id: '1234',
-                            text: 'Hello this is Alvic!!!!!',
-                            createdAt: DateTime.now().microsecondsSinceEpoch,
-                            author: Author(userName: 'alvicaranzo')));
-                    alignment: _messages[index].author.userName == 'pooja'
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    entity: _messages[index]);
-                  })),
-          ChatInput(),
-        ],
-      ),
-    );
+  final username = ModalRoute.of(context)!.settings.arguments as String;
+  return Scaffold(
+  appBar: AppBar(
+  backgroundColor: Colors.transparent,
+  elevation: 0,
+  title: Text('Hi $username!'),
+  actions: [
+  IconButton(
+  onPressed: () {
+  //TODO: Navigate back to LoginPage on logout
+  Navigator.pushReplacementNamed(context, '/');
+  print('Icon press');
+  },
+  icon: Icon(Icons.logout))
+  ],
+  ),
+  body: Column(
+  children: [
+  Expanded(
+  child: ListView.builder(
+  itemCount: _messages.length,
+  itemBuilder: (context, index) {
+  return ChatBubble(
+  alignment: _messages[index].author.userName == 'pooja'
+  ? Alignment.centerRight
+      : Alignment.centerLeft,
+  entity: _messages[index]);
+  })),
+  ChatInput(),
+  ],
+  ),
+  );
   }
-}
+  }
