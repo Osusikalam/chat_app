f179c7e686bbadd992b9c2080d45df4ec8ece9a0
Osusikalam <tribunalo.jonjie@gmail.com>
2025-05-30 00:48:32 +0800

    Why you need state management

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:chat_app/models/image_model.dart';
 import 'package:chat_app/repo/image_repository.dart';
+import 'package:chat_app/services/auth_service.dart';
 
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
@@ -84,6 +85,10 @@ class _ChatPageState extends State<ChatPage> {
                     return ChatBubble(
                         alignment:
                         _messages[index].author.userName == 'poojab26'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        alignment: _messages[index].author.userName ==
+                            AuthService().getUserName()
                             ? Alignment.centerRight
                             : Alignment.centerLeft,
                         entity: _messages[index]);
